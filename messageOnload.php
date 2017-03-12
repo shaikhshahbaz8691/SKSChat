080728b9809fdb0e0bdf5d817581881cb03d6c77
shaikhshahbaz8691 <shaikhshahbaz8691@gmail.com>
2017-03-12 10:17:59 +0530

    center tag
    centering message

--- messageOnload.php
+++ messageOnload.php
@@ -6,7 +6,7 @@ require_once("Connection.php");
 $query=mysqli_query($con,"select *from chat;");
 	while($row=mysqli_fetch_array($query))
 	{
-		echo "<p><b>".$row['message']."</b></p>";
+		echo "<p><b><center>".$row['message']."</center></b></p>";
 	}
 
 
